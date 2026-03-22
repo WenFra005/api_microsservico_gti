5470ca5bb6de063a3b3607f0850e5cf1402ac859
WenFra005 <wendellfrancisco2005@hotmail.com>
2026-03-21 21:34:39 -0300

    feat: Adicionar configuração inicial da API e integração com OpenRouter

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+import os
+
 from fastapi import FastAPI, HTTPException
+from openai import OpenAI
 from pydantic import BaseModel
 from urllib import request, error
 import json
@@ -8,6 +11,15 @@ app = FastAPI(
     description="Uma API para gerenciar a implantação de modelos de IA",
     version="1.0.0"
 )
+
+OPENROUTER_API_KEY = os.getenv("OPENROUTER_API_KEY")
+OPENROUTER_BASE_URL = os.getenv("OPENROUTER_API_URL")
+MODEL_NAME = "stepfun/step-3.5-flash:free"
+
+client = OpenAI(
+    base_url=OPENROUTER_BASE_URL,
+    api_key=OPENROUTER_API_KEY,
+) 
 
 class IntegracaoEntrada(BaseModel):
     texto: str
