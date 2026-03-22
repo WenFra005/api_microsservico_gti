24e0a8fe147b55010780e2c9c37936350b915b5b
WenFra005 <wendellfrancisco2005@hotmail.com>
2026-03-21 21:07:23 -0300

    feat: Adicionar classe IntegracaoEntrada para validação de entrada

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from urllib import request, error
+import json
 
 app = FastAPI(
     title="API de Implantacao de IA",
     description="Uma API para gerenciar a implantação de modelos de IA",
     version="1.0.0"
 )
+
+class IntegracaoEntrada(BaseModel):
+    texto: str
 
 @app.get("/")
 def read_root():
